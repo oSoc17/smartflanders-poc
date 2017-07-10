f31811b6074d15eca9f6720970a803852966b0af
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-10 16:12:46 +0200

    feat(parking.ts): add array to store historic vacant spaces data

--- src/app/models/parking.ts
+++ src/app/models/parking.ts
@@ -1,15 +1,17 @@
 export default class Parking {
   public name: string;
   public id: string;
-  public vacantSpaces: number;
+  public currentVacantSpaces: number;
+  public currentFilledSpaces: number;
   public totalSpaces: number;
-  public filledSpaces: number;
+  public historicVacantSpaces: Array<number>;
 
   constructor(name, id){
     this.name = name;
     this.id = id;
-    this.vacantSpaces = 0;
+    this.currentVacantSpaces = 0;
+    this.currentFilledSpaces = 0;
     this.totalSpaces = 0;
-    this.filledSpaces = 0;
+    this.historicVacantSpaces = [];
   }
 }
