d27cc31e49579b24f41df602282de70c0f58f43b
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-10 11:18:09 +0200

    feat(): broken branch, fix it!!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
-}
+
+  constructor(private dataService: DataService) {
+    dataService.get_data().then(response => {
+        console.log('Response in app.compontent.ts' + response)}); 
+    };
+  }
+
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,60 @@
 import { Injectable } from '@angular/core';
+//import { Http } from '@angular/http';
+import Ldfetch from 'ldfetch';
+import n3 from 'n3';
+//import Turtle from '../models/turtle';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+
 
 @Injectable()
 export class DataService {
+    private fetch;
 
-  constructor() { }
+
+  constructor() {       
+    this.fetch = <any>new Ldfetch();
+
+}
+
+  public get_data(): Promise<any>{
+      let amount = 0;
+      let max = 1000;
+      let state = new n3.Store();
+      return new Promise((resolve) => {
+    fetch.get('http://linked.open.gent/parking/').then(response => {
+      state.addTriples(response.triples);
+      state.addPrefixes(response.prefixes);
+      return response;
+    }).then(response => {
+      if (amount < max) {
+        let responseStore = new n3.Store();
+        responseStore.addTriples(response.triples);
+        responseStore.addPrefixes(response.prefixes);
+
+        let prevUrl = responseStore.getTriples(null,"hydra:previous")[0].object;
+        //return fetchUrl(prevUrl, amount+1, max, state);
+      } else {
+        return state;
+      }
+    }).then(res => resolve(res));
+  })
+
+  }
+
+  private extractData(res: any){
+    console.log("Response at this moment: \n " + res);
+    let state = new n3.Store();
+    state.addTriples(res.triples);
+    state.addPrefixes(res.prefixes);
+    console.log('State in extractData ::data.service.ts' + state);
+    return state;      
+
+  }
+  private handleError(error: Response | any){
+    console.log("There is an error in data.service.ts !");
+  return Observable.throw("Error");
+  }
 
 }
