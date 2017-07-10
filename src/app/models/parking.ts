7b3596a4c83c33ded8fedb3d236ac1ccc4241ea1
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-10 16:27:14 +0200

    style(app): fix indentation

--- src/app/models/parking.ts
+++ src/app/models/parking.ts
@@ -6,7 +6,7 @@ export default class Parking {
   public totalSpaces: number;
   public historicVacantSpaces: Array<number>;
 
-  constructor(name, id){
+  constructor(name, id) {
     this.name = name;
     this.id = id;
     this.currentVacantSpaces = 0;
