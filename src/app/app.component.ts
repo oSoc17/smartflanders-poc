f9526b862744289300b4e49ed5d3d0c3b5c05898
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-11 11:54:37 +0200

    created (stub) history method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ParkingDataService } from './services/parking-data.service';
 import n3 from 'n3';
 import Parking from './models/parking';
-import find from 'lodash/find';
 
 @Component({
   selector: 'app-root',
@@ -22,24 +21,11 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.N3Util = n3.Util;
     this.getParkings();
-    setInterval(this.getParkingData(), 30000);
   }
 
   private getParkings() {
     this.dataService.getParkings().then(parkings => {
       this.parkings = parkings;
     });
-  }
-
-private getParkingData() {
-  this.dataService.get_data().then(result => {
-    console.log(result);
-      result.forEach(element => {
-        const _parking = find(this.parkings, function(e){ return e.uri === element.subject});
-        if (_parking) {
-            _parking.currentVacantSpaces = this.N3Util.getLiteralValue(element.object);
-        }
-      });
-    })
   }
 }
--- src/app/services/parking-data.service.ts
+++ src/app/services/parking-data.service.ts
@@ -7,38 +7,52 @@ import 'rxjs/add/operator/catch';
 
 import Parking from '../models/parking';
 import ParkingHistory from '../models/parking-history';
+import Measurement from '../models/measurement';
 
 @Injectable()
 export class ParkingDataService {
+  private static getParking(uri, store): Parking {
+    const name = n3.Util.getLiteralValue(store.getTriples(uri, 'rdfs:label')[0].object);
+    const totalSpacesObj = store.getTriples(uri, 'datex:parkingNumberOfSpaces')[0].object;
+    const totalSpaces = parseInt(n3.Util.getLiteralValue(totalSpacesObj), 10);
+    return {
+      uri: uri,
+      name: name,
+      totalSpaces: totalSpaces
+    }
+  }
+
   constructor() {}
-
-  /*public get_data(): Promise<any> {
-    const fetch = <any> new ldfetch();
-    const amount = 0;
-    const max = 1000;
-    const state = new n3.Store();
-    const triples: Array<Triple> = [];
-
-    return new Promise((resolve) => {
-      fetch.get('http://linked.open.gent/parking/').then(response => {
-        state.addTriples(response.triples);
-        state.addPrefixes(response.prefixes);
-        const responseStore = new n3.Store();
-        responseStore.addTriples(response.triples);
-        responseStore.addPrefixes(response.prefixes);
-        // let prevUrl = responseStore.getTriples(null,"hydra:previous")[0].object;
-        // return fetchUrl(prevUrl, amount+1, max, state);
-        resolve(responseStore);
-      })
-    })
-  }*/
 
   public getParkingHistory(uri, from, to): Promise<ParkingHistory> {
     const fetch = new ldfetch();
 
     return new Promise((resolve) => {
-      resolve();
-    })
+      fetch.get('http://linked.open.gent/parking/').then(response => {
+        // Put all triples in store
+        const store = new n3.Store(response.triples, {prefixes: response.prefixes});
+
+        // Get all measurement triples that have this parking as subject
+        const measurementTriples = store.getTriples(uri, 'datex:parkingNumberOfVacantSpaces');
+        const timeframe: Measurement[] = [];
+        // Build a time frame for this file
+        measurementTriples.forEach(triple => {
+          const generatedAtTriple = store.getTriples(triple.graph, 'http://www.w3.org/ns/prov#generatedAtTime')[0];
+          const genTime = n3.Util.getLiteralValue(generatedAtTriple.object);
+          const value = n3.Util.getLiteralValue(triple.object);
+          timeframe.push({
+            timestamp: genTime,
+            value: value
+          });
+        });
+
+        const parking = ParkingDataService.getParking(uri, store);
+        resolve({
+          parking: parking,
+          timeframe: timeframe
+        });
+      });
+    });
   }
 
   public getParkings(): Promise<Parking[]> {
@@ -56,14 +70,7 @@ export class ParkingDataService {
         const parkings: Parking[] = [];
         parkingTriples.forEach(parking => {
           const uri = parking.subject;
-          const name = store.getTriples(uri, 'rdfs:label')[0].object;
-          const totalSpacesObj = store.getTriples(uri, 'datex:parkingNumberOfSpaces')[0].object;
-          const totalSpaces = parseInt(n3.Util.getLiteralValue(totalSpacesObj), 10);
-          parkings.push({
-            uri: uri,
-            name: name,
-            totalSpaces: totalSpaces
-          });
+          parkings.push(ParkingDataService.getParking(uri, store));
         });
 
         resolve(parkings);
