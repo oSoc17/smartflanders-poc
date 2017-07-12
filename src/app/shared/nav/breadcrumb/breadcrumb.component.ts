b6328231146e99e27fb1a2138c7a80202e1c4c71
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-12 17:32:40 +0200

    chore(breadcrumb): working breadcrumb for every detailpage

--- src/app/shared/nav/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/nav/breadcrumb/breadcrumb.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import Parking from './../../../models/parking'
 
 @Component({
   selector: 'app-breadcrumb',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breadcrumb.component.css']
 })
 export class BreadcrumbComponent implements OnInit {
+
+  @Input() private parking: Parking;
 
   constructor() { }
 
