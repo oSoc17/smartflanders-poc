6e365e38004230940a8becc4b8295994e1743c07
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-13 10:19:41 +0200

    chore(compare): generate compare parking card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FooterComponent } from './shared/footer/footer/footer.component';
 import { MomentModule } from 'angular2-moment';
 import { BreadcrumbComponent } from './shared/nav/breadcrumb/breadcrumb.component';
 import { ComparepageComponent } from './compare/comparepage/comparepage.component';
+import { ParkingCompareCardComponent } from './compare/comparepage/parking-compare-card/parking-compare-card.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { ComparepageComponent } from './compare/comparepage/comparepage.componen
     TimeFrameSelectComponent,
     FooterComponent,
     BreadcrumbComponent,
-    ComparepageComponent
+    ComparepageComponent,
+    ParkingCompareCardComponent
   ],
   imports: [
     BrowserModule,
