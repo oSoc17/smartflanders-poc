ba97adeb8b9ee2868562baf1918d5ba526727ad1
Ruben Thys <ruben.thys@student.odisee.be>
2017-07-13 10:00:29 +0200

    chore(compare): generate parking card

---
+++ src/app/compare/comparepage/parking-card/parking-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-card',
+  templateUrl: './parking-card.component.html',
+  styleUrls: ['./parking-card.component.css']
+})
+export class ParkingCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
