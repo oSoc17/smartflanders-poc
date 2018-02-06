1a746f2bb122e0e36bed5b32341e47aee34faf52
Arne Gevaert <arnegevaert@gmail.com>
2018-02-06 21:36:28 +0100

    Trying more things

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode } from '@angular/core';
+import './polyfills.ts';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-import './polyfills';
 
 if (environment.production) {
   enableProdMode();
